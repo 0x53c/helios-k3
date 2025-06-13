10823742e0ca138f32b28c385ea4c6e36d321f8d
The0x <0xvmx@proton.me>
2025-06-13 14:53:47 -0500

    fix: correct memory typo and update connection handling for master and worker nodes
    - Fixed typo in memory configuration from "memeory" to "memory".
    - Updated the connection arguments for the master node to use `pulumi.String()`.
    - Refactored the creation of worker nodes to properly handle IP and token retrieval.
    - Improved script formatting for clarity and correctness in the worker provisioning process.
    - Ensured proper export of master and worker node IPs and kubeconfig.

--- main.go
+++ main.go
@@ -5,18 +5,16 @@ import (
 	"fmt"
 
 	"github.com/pulumi/pulumi-command/sdk/go/command/remote"
-	"github.com/pulumi/pulumi-command/sdk/v3/go/command/remote"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
-	"gopkg.in/src-d/go-git.v4/plumbing/format/config"
+	"github.com/pulumi/pulumi/sdk/v3/go/pulumi/config"
 )
 
 const masterLimaConfig = `
 arch: "aarch64"
 cpus: 4
-memeory: "8GiB"
+memory: "8GiB"
 disk: "60GiB"
 `
-
 const workerLimaConfig = `
 arch: "aarch64"
 cpus: 2
@@ -30,33 +28,26 @@ func main() {
 		sshUser := conf.Require("ssh_user")
 		masterMacIP := conf.Require("master_mac_ip")
 
-		var workerMacIps []string
-		conf.RequireObject("worker_mac_ips", &workerMacIps)
-
-		masterMacConnect := remote.ConnectionArgs{
-			Host: masterMacIP,
+		var workerMacIPs []string
+		conf.RequireObject("worker_mac_ips", &workerMacIPs)
+		masterMacConnection := remote.ConnectionArgs{
+			Host: pulumi.String(masterMacIP),
 			User: pulumi.String(sshUser),
 		}
 
 		masterNode, err := remote.NewCommand(ctx, "provision-master-node", &remote.CommandArgs{
-			Connection: masterMacConnect,
+			Connection: masterMacConnection,
 			Create: pulumi.Sprintf(`
-			set -e
-			
-			limacl delete -f k3s-master || true
-
-			echo  '%s' > /tmp/k3-master.yaml
-			limactl start --name=k3-master /tmp/k3s-master.yaml
-
-			limactl shell k3s-master -- 'curl -sfL https://get.h3s.io | INSTALL_K3s_EXEC="--flannel-iface-limo0" sh -s -'
-			TOKEN=$(limactl shell k3s-master sduo cat /var/lib/rancher/k3s/server/node-token)
-			IP=$(limactl list k3s-master --json | jq -r .address)
-			
-			KUBECONFIG=$(limactl shell k3s-master sudo cat /etc/rancher/k3s/k3s.yaml | sed "s/127.0.0.1/$IP/g")
-
-			echo "{\"ip"\": \:$IP\" \"token"\, \"kubeconig\": \"$KUBECONFIG\"}"
+				set -e
+				limactl delete -f k3s-master || true
+				echo '%s' > /tmp/k3s-master.yaml
+				limactl start --name=k3s-master /tmp/k3s-master.yaml
+				limactl shell k3s-master -- 'curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--flannel-iface=lima0" sh -s -'
+				TOKEN=$(limactl shell k3s-master sudo cat /var/lib/rancher/k3s/server/node-token)
+				IP=$(limactl list k3s-master --json | jq -r .address)
+				KUBECONFIG=$(limactl shell k3s-master sudo cat /etc/rancher/k3s/k3s.yaml | sed "s/127.0.0.1/$IP/g")
+				echo "{\"ip\": \"$IP\", \"token\": \"$TOKEN\", \"kubeconfig\": \"$KUBECONFIG\"}"
 			`, masterLimaConfig),
-
 			Delete: pulumi.String("limactl delete -f k3s-master"),
 		}, pulumi.Timeouts(&pulumi.CustomTimeouts{Create: "20m"}))
 		if err != nil {
@@ -72,9 +63,64 @@ func main() {
 			return result, nil
 		}).(pulumi.MapOutput)
 
-		masterIP := masterOutput.MapIndex(pulumi.String("ip")).(pulumi.StringOutput)
-		masterToken := masterOutput.MapIndex(pulumi.String("token")).(pulumi.StringOutput)
-		kubeConfig := masterOutput.MapIndex(pulumi.String("kubeconfig")).(pulumi.StringOutput)
+		masterIP := pulumi.Sprintf("%s", masterOutput.MapIndex(pulumi.String("ip")))
+		masterToken := pulumi.Sprintf("%s", masterOutput.MapIndex(pulumi.String("token")))
+		kubeConfig := pulumi.Sprintf("%s", masterOutput.MapIndex(pulumi.String("kubeconfig")))
 
+		var workerNodeIPs []pulumi.StringInput
+
+		for i, workerMacIP := range workerMacIPs {
+			workerName := fmt.Sprintf("k3s-worker-%d", i)
+
+			workerMacConnection := remote.ConnectionArgs{
+				Host: pulumi.String(workerMacIP),
+				User: pulumi.String(sshUser),
+			}
+
+			createScriptAnyOutput := pulumi.All(masterIP, masterToken).ApplyT(func(args []interface{}) (string, error) {
+				masterIPStr := args[0].(string)
+				masterTokenStr := args[1].(string)
+
+				script := fmt.Sprintf(`
+					set -e
+					limactl delete -f %s || true
+					echo '%s' > /tmp/%s.yaml
+					limactl start --name=%s /tmp/%s.yaml
+					limactl shell %s -- 'curl -sfL https://get.k3s.io | K3S_URL=https://%s:6443 K3S_TOKEN="%s" INSTALL_K3S_EXEC="--flannel-iface=lima0" sh -'
+					IP=$(limactl list %s --json | jq -r .address)
+					echo "{\"ip\": \"$IP\"}"
+				`, workerName, workerLimaConfig, workerName, workerName, workerName, workerName, masterIPStr, masterTokenStr, workerName)
+
+				return script, nil
+			})
+
+			workerNode, err := remote.NewCommand(ctx, fmt.Sprintf("provision-worker-%d", i), &remote.CommandArgs{
+				Connection: workerMacConnection,
+				Create:     pulumi.Sprintf("%s", createScriptAnyOutput),
+				Delete:     pulumi.Sprintf("limactl delete -f %s", workerName),
+			}, pulumi.DependsOn([]pulumi.Resource{masterNode}), pulumi.Timeouts(&pulumi.CustomTimeouts{Create: "20m"}))
+			if err != nil {
+				return err
+			}
+
+			workerOutput := workerNode.Stdout.ApplyT(func(output string) (map[string]interface{}, error) {
+				var result map[string]interface{}
+				json.Unmarshal([]byte(output), &result)
+				return result, nil
+			}).(pulumi.MapOutput)
+
+			workerNodeIPs = append(workerNodeIPs, pulumi.Sprintf("%s", workerOutput.MapIndex(pulumi.String("ip"))))
+		}
+
+		allWorkerIPs := make([]interface{}, len(workerNodeIPs))
+		for i, ip := range workerNodeIPs {
+			allWorkerIPs[i] = ip
+		}
+
+		ctx.Export("masterNodeIP", masterIP)
+		ctx.Export("workerNodeIPs", pulumi.All(allWorkerIPs...))
+		ctx.Export("kubeconfig", pulumi.ToSecret(kubeConfig))
+
+		return nil
 	})
 }
